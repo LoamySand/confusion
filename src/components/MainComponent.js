16ce900d0b02874e6b72b02433bdb018662ad971
LoamySand <laneeboyd@gmail.com>
2023-04-14 14:48:17 -0500

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-import DishDetail from './DishDetailComponent';
 import { DISHES } from '../shared/dishes';
-import Header from './HeaderComponent';
-import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
 class Main extends Component {
     constructor(props) {
         super(props);
@@ -16,14 +17,20 @@ class Main extends Component {
         this.setState({ selectedDish: dishId});
     }
     render() {
+        const HomePage = () => {
+            return (
+                <Home/>
+            );
+        }
         return (
             <div>
-                <Header />
-                <Menu dishes={this.state.dishes} onClick={(dishId) =>
-                    this.onDishSelect(dishId)} />
-                <DishDetail dish={this.state.dishes.filter((dish) => dish.id ===
-                    this.state.selectedDish)[0]} />
-                <Footer />
+                <Header/>
+                <Switch>
+                    <Route path='/home' component={HomePage}/>
+                    <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes}/>}/>
+                    <Redirect to='/home'/>
+                </Switch>
+                <Footer/>
             </div>
         );
     }
