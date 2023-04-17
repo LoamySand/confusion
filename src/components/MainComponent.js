fe87d660d75e1549f30424df1033322b7b13c674
LoamySand <laneeboyd@gmail.com>
2023-04-17 14:51:09 -0500

    Single Page Applications Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
 import Home from './HomeComponent';
+import Menu from './MenuComponent';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
@@ -9,6 +9,8 @@ import Contact from './ContactComponent';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
+
+import DishDetail from "./DishDetailComponent";
 
 class Main extends Component {
     constructor(props) {
@@ -20,9 +22,6 @@ class Main extends Component {
             leaders:LEADERS
         };
     }
-    onDishSelect(dishId) {
-        this.setState({ selectedDish: dishId});
-    }
     render() {
         const HomePage = () => {
             return (
@@ -33,13 +32,27 @@ class Main extends Component {
                 />
             );
         }
+        const DishWithId=({match})=>{
+            if (parseInt(match.params.dishId,10)===null){
+                return ({Menu});
+            } else {
+                return (
+                    <DishDetail dish={this.state.dishes.filter((dish) => dish.id ===
+                        parseInt(match.params.dishId, 10))[0]}
+                                comments={this.state.comments.filter((comment) =>
+                                    comment.dishId === parseInt(match.params.dishId, 10))}/>
+                );
+            }
+        };
+
         return (
             <div>
                 <Header/>
                 <Switch>
                     <Route path='/home' component={HomePage}/>
-                    <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes}/>}/>
-                    <Route exact path='/contacts' component={Contact}/>
+                    <Route exact path='/menu' component={()=><Menu dishes={this.state.dishes}/>}/>
+                    <Route path='/menu/:dishId' component={DishWithId} />
+                    <Route exact path='/contactus' component={Contact}/>
                     <Redirect to='/home'/>
                 </Switch>
                 <Footer/>
