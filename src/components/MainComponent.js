d5372e0e4a68c76c2bfbb35a8264003ecb65407e
LoamySand <laneeboyd@gmail.com>
2023-04-17 15:26:16 -0500

    Complete Assignment 6

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -11,6 +11,7 @@ import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
 
 import DishDetail from "./DishDetailComponent";
+import About from "./AboutComponent";
 
 class Main extends Component {
     constructor(props) {
@@ -53,6 +54,8 @@ class Main extends Component {
                     <Route exact path='/menu' component={()=><Menu dishes={this.state.dishes}/>}/>
                     <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path='/contactus' component={Contact}/>
+                    {/*<Route exact path='/aboutus' component={About}/>*/}
+                    <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
                     <Redirect to='/home'/>
                 </Switch>
                 <Footer/>
